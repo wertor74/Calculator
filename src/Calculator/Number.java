db3b9d025cc98c5ba192d4e7fe2080e81c8a280e
wertor <wertor74@gmail.com>
2022-04-05 21:42:42 +0300

    CalcStart

---
+++ src/Calculator/Number.java
@@ -0,0 +1,23 @@
+package Calculator;
+// операнды и результат
+class Number {
+    public String strValue; // строковое значение
+    public int intValue; // числовое значение
+    public boolean romanNumber; // является ли римским числом
+
+    public Number (int intValue) {
+        this.intValue = intValue;
+        romanNumber = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Результат: " + strValue;
+    }
+    public int getIntValue () {
+        return intValue;
+    }
+    public boolean getRomanNumber () {
+        return romanNumber;
+    }
+}
